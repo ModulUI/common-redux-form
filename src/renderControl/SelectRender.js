9e79bd66c26731e412425c7f556263da185c0e53
Qireel <kirill.karmachev@gmail.com>
2019-01-10 18:16:48 +0700

    - спред внутри объекта убрал

--- src/renderControl/SelectRender.js
+++ src/renderControl/SelectRender.js
@@ -43,7 +43,7 @@ class SelectRender extends React.Component {
                 className={classNames}
                 {...input}
                 {...selectOptions}
-                tooltipProps={{...tooltip}}
+                tooltipProps={tooltip}
                 onFocus={this.onFocus}
                 valueKey={valueKey}
                 onChange={this.onChangeSelect}
